a0e115436bf47ed9a13c22c98c961d85094b1290
Dmitriy Shitskov <ds@pay-me.ru>
2021-08-09 10:01:43 +0300

    feature: zookeeper additional settings

--- types/clickhouse_replication.pp
+++ types/clickhouse_replication.pp
@@ -1,4 +1,9 @@
 # lint:ignore:2sp_soft_tabs
 type Clickhouse::Clickhouse_replication = Struct[{zookeeper_servers => Array[Pattern['[^\:]+:[0-9]{1,5}']],
-                                                  Optional[macros]  => Hash[String, Any]}]
+                                                  Optional[macros]  => Hash[String, Any],
+                                                  Optional[session_timeout] => Integer,
+                                                  Optional[operation_timeout] => Integer,
+                                                  Optional[root] => String,
+                                                  Optional[user] => String,
+                                                  Optional[password] => String}]
 # lint:endignore
